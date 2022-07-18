e4c866d2fbb075d0f23c1a6d063af8374031ef00
bree <me@bree.dev>
2022-07-18 17:34:13 -0400

    add registries registry datagen
    outputs the list of ids for each registry

--- src/main/java/dev/bree/datadumper/datagen/DataDumperDatagen.java
+++ src/main/java/dev/bree/datadumper/datagen/DataDumperDatagen.java
@@ -3,6 +3,7 @@ package dev.bree.datadumper.datagen;
 import dev.bree.datadumper.datagen.provider.AttributeDataProvider;
 import dev.bree.datadumper.datagen.provider.FoodDataProvider;
 import dev.bree.datadumper.datagen.provider.ItemDataProvider;
+import dev.bree.datadumper.datagen.provider.RegistriesDataProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
@@ -12,5 +13,6 @@ public class DataDumperDatagen implements DataGeneratorEntrypoint {
         fabricDataGenerator.addProvider(ItemDataProvider::new);
         fabricDataGenerator.addProvider(FoodDataProvider::new);
         fabricDataGenerator.addProvider(AttributeDataProvider::new);
+        fabricDataGenerator.addProvider(RegistriesDataProvider::new);
     }
 }
---
+++ src/main/java/dev/bree/datadumper/datagen/provider/RegistriesDataProvider.java
@@ -0,0 +1,57 @@
+package dev.bree.datadumper.datagen.provider;
+
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import dev.bree.datadumper.datagen.serializer.EntityAttributeModifierSerializer;
+import dev.bree.datadumper.datagen.serializer.EntityAttributeSerializer;
+import dev.bree.datadumper.datagen.serializer.IdentifierSerializer;
+import dev.bree.datadumper.datagen.serializer.ItemSerializer;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.data.DataProvider;
+import net.minecraft.data.DataWriter;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.HashMap;
+
+public class RegistriesDataProvider implements DataProvider {
+    protected final FabricDataGenerator dataGenerator;
+
+    public RegistriesDataProvider(FabricDataGenerator dataGenerator) {
+        this.dataGenerator = dataGenerator;
+    }
+
+    @Override
+    public String getName() {
+        return "Registry";
+    }
+
+    @Override
+    public void run(DataWriter writer) throws IOException {
+        final var registries = new HashMap<String, JsonObject>();
+
+        var gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(Identifier.class, new IdentifierSerializer());
+        var gson = gsonBuilder.create();
+
+
+        for (var id : Registry.REGISTRIES.getIds()) {
+            final var registry = Registry.REGISTRIES.get(id);
+            final var namespace = registries.computeIfAbsent(id.getNamespace(), (_namespace) -> new JsonObject());
+            namespace.add(id.toString(), gson.toJsonTree(registry.getIds()));
+        }
+
+        for (final var entry : registries.entrySet()) {
+            DataProvider.writeToPath(writer, entry.getValue(), getOutputPath(entry.getKey()));
+        }
+    }
+
+    private Path getOutputPath(String namespace) {
+        return dataGenerator.getOutput().resolve("data/%s/registries.json".formatted(namespace));
+    }
+}
